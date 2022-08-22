d3653983e771ebbac30ae9455858fbd18b5ddfbd
sandaru97 <sandaruadithya@gmail.com>
2022-08-22 13:44:35 +1000

    first comit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//imort simple knob
+import Knob from "react-simple-knob";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Knob
+      name="Volume"
+      unit="dB"
+      defaultPercentage={0.7}
+      onChange={console.log}
+      bg="grey"
+      fg="black"
+      mouseSpeed={5}
+      transform={p => parseInt(p * 50, 10) - 50}
+      style={{
+        margin: "20%",
+        height: "100px",
+        fontFamily: "Arial",
+        color: "red" // Sets font color of value and knob name
+      }} /></View>
   );
 }
 
